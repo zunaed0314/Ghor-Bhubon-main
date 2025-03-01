83467fcfccebf0ba26e7a6cf09b7398d3f3baee1
zunaed0314 <zunaedaman@gmail.com>
2025-03-01 15:10:12 +0600

    bruh

--- Ghor-Bhubon/Controllers/AdminController.cs
+++ Ghor-Bhubon/Controllers/AdminController.cs
@@ -1,11 +1,26 @@
-﻿using Ghor_Bhubon.Models;
+﻿using Ghor_Bhubon.Data;
+using Ghor_Bhubon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
 
 namespace Ghor_Bhubon.Controllers
 {
     public class AdminController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AdminController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
         public IActionResult AdminDashBoard()
         {
             return View();
@@ -18,9 +33,10 @@ namespace Ghor_Bhubon.Controllers
         }
 
 
-        public IActionResult ManageUsers()
+        public async Task<IActionResult> ManageUsers()
         {
-            return View();
+            var user = _context.Users.ToList();
+            return View(user);
         }
 
         public IActionResult Profile()
